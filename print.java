2dcc9b66d9bd9cce06a53502ecb857686a1b6d8f
Barik <bari1999smab@gmail.com>
2024-07-09 08:07:48 +0100

    added the print java with register branch

---
+++ print.java
@@ -0,0 +1,20 @@
+class print{
+    public static void main(String[] args) {
+        System.err.println("hello java :");
+        System.err.println("hello java :");
+        System.err.println("hello java :");
+        System.err.println("hello java :");
+        System.err.println("hello java :");
+        System.err.println("hello java :");
+        System.err.println("hello java :");
+
+        System.err.println("hello java :");
+        System.err.println("hello java :");
+        System.err.println("hello java :");
+        System.err.println("hello java :");
+        System.err.println("hello java :");
+        System.err.println("hello java :");
+        System.err.println("hello java :");
+
+    }
+}
